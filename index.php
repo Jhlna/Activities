685b86e8390ac29e9fe6618d369e8088ec11e6a5
Jhlna <76999685+Jhlna@users.noreply.github.com>
2021-01-21 10:58:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,19 +8,19 @@
 <br> Let us always pray for our health.
 <br><br>
  
-<p align = "center"> <img src = "C:\Users\ACER\Pictures\week 3\pics\Sun.png" width = "210", length = "210"> </img> <br>
+<p align = "center"> <img src = "pics\Sun.png" width = "210", length = "210"> </img> <br>
 The sunshine is so bright
 <br> It gives hope to everyone
 <br><br>
 
-<img src = "C:\Users\ACER\Pictures\week 3\pics\Circle.png" width = "300", length = "300"> </img> <br>
+<img src = "pics\Circle.png" width = "300", length = "300"> </img> <br>
 It's a small
 <br>
 <b> Wolrd after all </b>
 <br> <br> </p>
 
 <p align = "right">
-<img src= "C:\Users\ACER\Pictures\week 3\pics\Smiley.png" width = "450", length = "450"> </img> <br>
+<img src= "pics\Smiley.png" width = "450", length = "450"> </img> <br>
 <b> Always wear a smile, </b>
 <br> you might inspire somebody
 <br> with it. </p>
@@ -37,7 +37,7 @@ It's a small
 				</h6>
 			</i>
 			<p align="right">
-				<img src="C:\Users\ACER\Pictures\week 3\pics\09.jpg" width="210" height="210">	
+				<img src="pics\09.jpg" width="210" height="210">	
 			</p>	
 		</font>
 		<font color="black" face="arial">
@@ -47,7 +47,7 @@ It's a small
 				</h5>
 			</i>
 			<p align="right">
-				<img src="C:\Users\ACER\Pictures\week 3\pics\umbrella.jpg" width="210" height="210">
+				<img src="pics\umbrella.jpg" width="210" height="210">
 			</p>
 		</font>
 		<font color="red" face="arial">
@@ -57,7 +57,7 @@ It's a small
 				</h4>
 			</i>
 			<p align="right">
-				<img src="C:\Users\ACER\Pictures\week 3\pics\02.jpg" width="210" height="210">
+				<img src="pics\02.jpg" width="210" height="210">
 			</p>
 		</font>
 		<font color="black" face="arial">
@@ -65,7 +65,7 @@ It's a small
 				I can do nothing about the past. My future is immediate.
 			</h3>
 			<p align="right">
-				<img src="C:\Users\ACER\Pictures\week 3\pics\13.png" width="210" height="210">
+				<img src="pics\13.png" width="210" height="210">
 			</p>
 		</font>
 		<font color="red" face="arial">
@@ -73,7 +73,7 @@ It's a small
 				I will grasp it in both hands and carry it with running feet.
 			</h3>
 			<p align="right">
-				<img src="C:\Users\ACER\Pictures\week 3\pics\15.png" width="210" height="210">
+				<img src="pics\15.png" width="210" height="210">
 			</p>
 		</font>
 		<font color="black" face="arial">
@@ -81,7 +81,7 @@ It's a small
 				When i am faced with the choice of doing nothing or doing something,
 			</h2>
 			<p align="right">
-				<img src="C:\Users\ACER\Pictures\week 3\pics\16.jpg" width="210" height="210">		
+				<img src="pics\16.jpg" width="210" height="210">		
 			</p>
 		</font>
 		<font color="red" face="arial">
@@ -89,7 +89,7 @@ It's a small
 				I will always choose to act! I seize this moment.
 			</h1>
 			<p align="right">
-				<img src="C:\Users\ACER\Pictures\week 3\pics\22.jpg" width="210" height="210">
+				<img src="pics\22.jpg" width="210" height="210">
 			</p>
 		</font>
 		<font color="black" face="arial">
@@ -97,7 +97,7 @@ It's a small
 			I choose now." ~
 			</h1>
 			<p align="right">
-				<img src="C:\Users\ACER\Pictures\week 3\pics\anaderumbrella.jpg" width="210" height="210">
+				<img src="pics\anaderumbrella.jpg" width="210" height="210">
 			</p>
 		</font>
 		<font color="red" face="arial">
